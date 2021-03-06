761e6d6c39b3f0cab3860ed561aba9d6a229893d
Huanderson Machado <huandersonmachado@gmail.com>
2021-03-06 12:41:39 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Boilerplate Laravel 8 API
+
+##Tecnologias
+
+- Laravel 8
+- JWT
+- scaffold Auth
+- ClockWork
+- Tradução PT-BR
---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use PHPUnit\Exception;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'signUp']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required|min:8'
+        ]);
+
+
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => __('auth.failed')], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function signUp(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required|max:55',
+                'email' => 'email|required|unique:users',
+                'password' => 'required|min:8'
+            ]);
+
+            $user = app(User::class);
+
+            $userCreated = $user->create($validatedData);
+
+            $accessToken = auth('api')->login($userCreated);
+
+            return $this->respondWithToken($accessToken);
+
+        } catch(Exception $exception) {
+            return response()->json([
+                'message' => $exception->getMessage()
+            ]);
+        }
+    }
+}
